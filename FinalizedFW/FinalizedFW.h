a79897514c57f7f8dc3fee0ade3284662730cec6
saddamvs <saddam.husain@vectoscalar.com>
2016-06-08 17:02:24 +0530

    Initial Commit

---
+++ FinalizedFW/FinalizedFW.h
@@ -0,0 +1,19 @@
+//
+//  FinalizedFW.h
+//  FinalizedFW
+//
+//  Created by VS-Saddam Husain-MacBookPro on 08/06/16.
+//  Copyright © 2016 VS-Saddam Husain-MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FinalizedFW.
+FOUNDATION_EXPORT double FinalizedFWVersionNumber;
+
+//! Project version string for FinalizedFW.
+FOUNDATION_EXPORT const unsigned char FinalizedFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinalizedFW/PublicHeader.h>
+
+
